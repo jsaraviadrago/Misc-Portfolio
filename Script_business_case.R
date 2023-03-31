7bd45f6b708f3065d0607126cefdae1e248f6f7e
JC Saravia <jsaraviadrago@gmail.com>
2023-03-31 12:19:54 -0500

    Update cargando las tablas

---
+++ Script_business_case.R
@@ -0,0 +1,9 @@
+# Business case driblab
+
+library(dplyr)
+
+data_players <- readxl::read_xlsx("/Users/jsaraviadrago/Documents/Driblab_2023/Busines case/Libro2.xlsx")
+  
+data_transfers <- readxl::read_xlsx("/Users/jsaraviadrago/Documents/Driblab_2023/Busines case/Historial transfers - Perú.xlsx")
+
+
