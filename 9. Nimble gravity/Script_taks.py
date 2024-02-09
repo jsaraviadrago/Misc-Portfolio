bea751a5889c7b986ced31c41b67ab8e95e25acb
JC Saravia <jsaraviadrago@gmail.com>
2024-02-09 12:53:00 -0500

    Setting up nimble gravity

---
+++ 9. Nimble gravity/Script_taks.py
@@ -0,0 +1,6 @@
+
+
+# s3_uri=s3://ng-data-science-interviews/clickstream2
+# s3_region= us-east-2
+#s3_access_key_id=AKIASQRQO2RJDCVD7SAA
+# s3_secret_access_key=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
