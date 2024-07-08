a42dc0f5fe77e5cf0a3e6eb5c3765635385f9176
JC Saravia <jsaraviadrago@gmail.com>
2024-07-08 09:51:27 -0500

    update

--- 0. Fun 3D plots and art/Script_flametree.R
+++ 0. Fun 3D plots and art/Script_flametree.R
@@ -4,16 +4,22 @@ library(viridis)
 library(ggplot2)
 
 dat <- flametree_grow(seed = 1, time = 5, 
-                      scale = 2,
+                      scale = c(2,4),
                       split = 5) # data structure
-img <- flametree_plot(tree = dat,
-                      palette = "viridis::viridis")          # ggplot object
 
+shades <- c("#A06AB4", "#FFD743", "#07BB9C", "blue")
+
+img <- flametree_plot(data = dat,
+                      background = "grey",
+                      palette = shades)          # ggplot object
+
+n_colors <- 10
+colors <- viridis(n_colors)
 
 dat2 <- flametree_grow(seed = 1, time = 6, 
-                       scale = 2,
-                       split = 4) # data structure
-img2 <- flametree_plot(tree = dat,
-                       palette = "grDevices::rainbow")  
+                       scale = c(2,4,6,8),
+                       split = 2) # data structure
+img2 <- flametree_plot(data = dat,
+                       palette = colors)  
 
 flametree_save(img2, 'C:\\Users\\JuanCarlosSaraviaDra\\img2.png')
