912a11a8225e9f40be9709e3ee363242092c1944
JC Saravia <jsaraviadrago@gmail.com>
2024-07-16 10:11:06 -0500

    update

--- 0. Fun 3D plots and art/Scripts_triforce.R
+++ 0. Fun 3D plots and art/Scripts_triforce.R
@@ -1,6 +1,7 @@
 library(sf)
 library(rayshader)
 library(rayrender)
+library(ggplot2)
 # Coordinate seperate in grids for simplicity
 #x <- c(0,1,2,3,1,2,4)
 #y <- c(0,3,0,3,3,6,0)
@@ -15,8 +16,24 @@ multipoint <-  st_multipoint(matrix(c(0,10,20,30,10,20,40,0,30,0,30,30,60,0),
 # Creating the polygon
 polyg <-  st_cast(multipoint, "POLYGON")
 
-# Plotting the squares
-plot(polyg, col = "Gold")
+# Converting to sf object
+polyg_sf <- st_sfc(polyg)
+
+# Creating a data frame from the sf object
+df <- st_as_sf(data.frame(geometry = polyg_sf))
+
+# Plotting with ggplot
+triforce <- ggplot() +
+  geom_sf(data = df, fill = "gold", color = "black")+ 
+  theme(panel.background = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.line.x = element_blank(),
+        axis.line.y = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.ticks = element_blank(),
+        legend.title = element_blank(), legend.position="none")
 
 
 
